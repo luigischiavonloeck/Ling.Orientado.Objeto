fa91020995fe5ce319d75b70e6a09f55d546a518
LuigiLoeck <luigisloeck@gmail.com>
2023-08-23 23:43:18 -0300

    controller update

--- objetivo_2/src/Controller/ControllerAutomovel.java
+++ objetivo_2/src/Controller/ControllerAutomovel.java
@@ -2,15 +2,18 @@ package Controller;
 
 import Model.Automovel;
 
+import java.util.*;
+
+
 public class ControllerAutomovel {
     public static void main(String[] args) {
         System.out.println("======= exercicio 1 =======");
-        Automovel at1 = new Automovel(1L,"Teste","ABCDE-123","Azul",4,"Diesel",5000,"SJKFGSEJKGVHSDV", 1500);
+        Automovel at1 = new Automovel(1L,"7834658345","ABCDE-123","Azul",4,"Diesel",5000,"SJKFGSEJKGVHSDV", 1500);
         Automovel at2 = new Automovel();
         System.out.println(at1);
         System.out.println(at2);
         at2.setId(2L);
-        at2.setRenavan("Renault");
+        at2.setRenavan("873465367");
         at2.setPlaca("12345-ABC");
         at2.setCor("Amarelo");
         at2.setNum_rodas(6);
@@ -26,5 +29,41 @@ public class ControllerAutomovel {
         System.out.println(at2.getQuilometragem());
         System.out.println(at2.getChasi());
         System.out.println(at2.getValor_locacao());
+
+        System.out.println("====== Exercicio 2 =======");
+        Automovel at3 = new Automovel(3L,"68678568","DFRTJ-389","Roxo",4,"Etanol",1564,"IYKJTSDEGYUDRH", 1000);
+        Automovel at4 = new Automovel(4L,"87235274","8H7J5-F46","Verde",8,"Etanol",57866,"KASHGEBISDUGR", 7500);
+        Automovel at5 = new Automovel(5L,"23875623","48W7M-TY5","Azul",2,"Gasolina",8637,"UJEHRGHKDRGJERG", 500);
+        Automovel at6 = at1;
+        System.out.println("------ List -----");
+        List<Automovel> automovelList = new ArrayList<>();
+        automovelList.add(at1);
+        automovelList.add(at2);
+        automovelList.add(at3);
+        automovelList.add(at4);
+        automovelList.add(at5);
+        automovelList.add(at6);
+        System.out.println(automovelList);
+
+        System.out.println("--- Procurar o objeto de id3");
+        Automovel findAutomovel = automovelList.stream().filter(automovel -> automovel.getId()==3L).findAny().orElse(null);
+        System.out.println(findAutomovel);
+
+        System.out.println("--- Ordenar a lista");
+        automovelList.sort(Comparator.comparing(Automovel::getId).reversed());
+        System.out.println(automovelList);
+
+        System.out.println("------ Map ------");
+        Map<Long,Automovel> automovelMap = new HashMap<>();
+        automovelMap.put(at1.getId(), at1);
+        automovelMap.put(at2.getId(), at2);
+        automovelMap.put(at3.getId(), at3);
+        automovelMap.put(at4.getId(), at4);
+        automovelMap.put(at5.getId(), at5);
+        automovelMap.put(at6.getId(), at6);
+        System.out.println(automovelMap);
+
+        System.out.println("--- Procurar o objeto de id3");
+        System.out.println(automovelMap.get(3L));
     }
 }
--- objetivo_2/src/Controller/ControllerLocacao.java
+++ objetivo_2/src/Controller/ControllerLocacao.java
@@ -3,9 +3,11 @@ package Controller;
 import Model.Locacao;
 
 import java.time.*;
+import java.util.*;
 
 public class ControllerLocacao {
     public static void main(String[] args) {
+        System.out.println("======= exercicio 1 =======");
         Locacao lc1 = new Locacao(1L,LocalDate.of(2019,8,5),LocalTime.of(18,34,7),LocalDate.of(2020,1,17),LocalTime.of(16,17,11),2560,1900,3700,true);
         Locacao lc2 = new Locacao();
         System.out.println(lc1);
@@ -27,5 +29,42 @@ public class ControllerLocacao {
         System.out.println(lc2.getDataDevolucao());
         System.out.println(lc2.getHoraDevolucao());
         System.out.println(lc2.isDevolvido());
+
+        System.out.println("====== Exercicio 2 =======");
+        Locacao lc3 = new Locacao(3L,LocalDate.of(2017,9,1),LocalTime.of(6,23,37),LocalDate.of(2018,12,1),LocalTime.of(5,43,23),3487,7634,8673,false);
+        Locacao lc4 = new Locacao(4L,LocalDate.of(2018,6,23),LocalTime.of(23,54,6),LocalDate.of(2023,8,17),LocalTime.of(18,23,34),9834,7835,3852,false);
+        Locacao lc5 = new Locacao(5L,LocalDate.of(2022,2,15),LocalTime.of(11,12,23),LocalDate.of(2022,3,27),LocalTime.of(12,59,2),5623,3468,6257,true);
+        Locacao lc6 = lc1;
+        System.out.println("------ List -----");
+        List<Locacao> locacaoList = new ArrayList<>();
+        locacaoList.add(lc1);
+        locacaoList.add(lc2);
+        locacaoList.add(lc3);
+        locacaoList.add(lc4);
+        locacaoList.add(lc5);
+        locacaoList.add(lc6);
+        System.out.println(locacaoList);
+
+        System.out.println("--- Procurar o objeto de id3");
+        Locacao findLocacao = locacaoList.stream().filter(locacao -> locacao.getId()==3L).findAny().orElse(null);
+        System.out.println(findLocacao);
+
+        System.out.println("--- Ordenar a lista");
+        locacaoList.sort(Comparator.comparing(Locacao::getId).reversed());
+        System.out.println(locacaoList);
+
+        System.out.println("------ Map ------");
+        Map<Long,Locacao> locacaoMap = new HashMap<>();
+        locacaoMap.put(lc1.getId(), lc1);
+        locacaoMap.put(lc2.getId(), lc2);
+        locacaoMap.put(lc3.getId(), lc3);
+        locacaoMap.put(lc4.getId(), lc4);
+        locacaoMap.put(lc5.getId(), lc5);
+        locacaoMap.put(lc6.getId(), lc6);
+        System.out.println(locacaoMap);
+
+        System.out.println("--- Procurar o objeto de id3");
+        System.out.println(locacaoMap.get(3L));
+
     }
 }
