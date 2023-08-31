0cc1f05eacd680b7a330e97c991d83adac575254
aluno <aluno@email.com>
2023-08-31 11:27:01 -0300

    main class

---
+++ objetivo_2/listaHM1_o2/src/Controller/FuncionarioController.java
@@ -0,0 +1,7 @@
+package Controller;
+
+public class FuncionarioController {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ objetivo_2/listaHM1_o2/src/Model/Funcionario.java
@@ -0,0 +1,35 @@
+package Model;
+
+public class Funcionario {
+    protected String nome;
+    protected double salario;
+    protected double bonus;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getBonus() {
+        return salario*(bonus/100);
+    }
+}
